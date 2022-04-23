f5210fb9c671b4f454f9a4f80c3afbf2f5dfdf1f
Surender <tsuren1998@outlook.com>
2022-04-23 13:17:40 +0530

    Spring Autowiring, Bean scope and ApplicationContextAware

--- Demo_Java1/src/org/surender/demo/Triangle2.java
+++ Demo_Java1/src/org/surender/demo/Triangle2.java
@@ -2,13 +2,19 @@ package org.surender.demo;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.forwardedUrl;
 
+
 import java.util.List;
 
-public class Triangle2 {
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+public class Triangle2 implements ApplicationContextAware{
 	
 	public Point point1;
 	public Point point2;
 	public Point point3;
+	public ApplicationContext context = null;
 
 	public Point getPoint1() {
 		return point1;
@@ -36,5 +42,17 @@ public class Triangle2 {
 
 	public void draw() {
 		System.out.println("Triangle Drawn at ( "+getPoint1().xPoint+", "+getPoint1().yPoint+") and ("+getPoint2().xPoint+", "+getPoint2().yPoint+") and ("+getPoint3().xPoint+", "+getPoint3().yPoint+")");
+	}
+
+	
+	/*
+	 * This method is call once the beans are early loaded and it sets the context (triangle2_singleton object).
+	 * So now we can call the required bean and make it implement its respy., scope irrespective of its parent's scope 
+	 */
+	@Override
+	public void setApplicationContext(ApplicationContext context) throws BeansException {
+		System.out.println("Here!..."+context);
+		this.context =  context;
+		context.getBean("point1");
 	}
 }

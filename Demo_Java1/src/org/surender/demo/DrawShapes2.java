957e89f3e78a6e42a28304fa6475e6a8eb9a2cee
Surender <tsuren1998@outlook.com>
2022-04-23 12:42:29 +0530

    Spring Autowiring and Bean Scope

--- Demo_Java1/src/org/surender/demo/DrawShapes2.java
+++ Demo_Java1/src/org/surender/demo/DrawShapes2.java
@@ -11,6 +11,13 @@ public class DrawShapes2 {
 		//Since we are using aliases in spring.xml you can use the other name too
 		//Triangle1 triangle1 = (Triangle1) beanFactory.getBean("Triangle1");
 		Triangle2 triangle2 = (Triangle2) beanFactory.getBean("Triangle-autowire");
+		triangle2.point1.setxPoint(50);
 		triangle2.draw();
+		
+		//By Default the spring follows the singleton pattern that is any number of getBeans() for the same bean will return the same object
+		//The other pattern is called prototype which is the exact opposite of it
+		Triangle2 triangle2_singleton = (Triangle2) beanFactory.getBean("Triangle-autowire");
+		triangle2_singleton.draw();
 	}
 }
+ 
